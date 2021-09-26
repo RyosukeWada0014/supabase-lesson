83ba40f2981568c421ab5f269f101bd0a618a7f7
RyosukeWada0014 <fbdp.14@gmail.com>
2021-09-26 23:58:11 +0900

    ✨ TitleListコンポーネントをindex.tsxにて取り込む

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,58 @@
-import type { ReactNode } from "react";
-import type { NextPage } from "next";
-import { Auth, Button, IconLogOut, Typography } from "@supabase/ui";
+import { ReactNode, useCallback, useEffect, useState } from "react";
+import { NextPage } from "next";
+import { Auth, Button, IconLogOut } from "@supabase/ui";
 import { client } from "src/libs/supabase";
 import { LayoutWrapper } from "src/components/LayoutWrapper";
+import { Title, TitleList } from "src/components/TitleList";
 
 type Props = {
   children: ReactNode;
 };
 
+const getTitles = async () => {
+  const { data, error } = await client
+    .from("manga_title")
+    .select("*")
+    .order("title");
+
+  if (!error && data) {
+    return data;
+  }
+  return [];
+};
+
 const Container = (props: Props) => {
   const { user } = Auth.useUser();
+  const [text, setText] = useState<string>("");
+  const [titles, setTitles] = useState<Title[]>([]);
+
+  const getTitleList = useCallback(async () => {
+    const data = await getTitles();
+    setTitles(data);
+  }, []);
+
+  useEffect(() => {
+    getTitleList();
+  }, [user, getTitleList]);
 
   if (user) {
     return (
       <div>
+        <div className="flex justify-center gap-2 p-4">
+          <input
+            className="w-full h-12 px4 bg-white border border-gray-300 rounded shadow appearance-none hover:border-gray-700"
+            placeholder="Filtering text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <TitleList
+          titles={titles}
+          uuid={user.id}
+          getTitleList={getTitleList}
+          filterText={text}
+        />
         <div className="flex justify-end mx-2 my-4">
-          <Typography.Text>Signed in: {user.email}</Typography.Text>
           <Button
             size="medium"
             icon={<IconLogOut />}
@@ -27,6 +64,7 @@ const Container = (props: Props) => {
       </div>
     );
   }
+  
   return <div>{props.children}</div>;
 };
 
